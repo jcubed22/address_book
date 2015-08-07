1737dd17fcd3357db9a0c79632f8d0818ca17e23
Jordan Johansen <jordanjohan@gmail.com>
2015-08-07 09:45:13 -0700

    Create Contact class.

---
+++ src/Contact.php
@@ -0,0 +1,60 @@
+<?php
+class Contact
+{
+    private $name
+    private $phone
+    private $address
+
+    function __construct($name, $phone, $address)
+    {
+        $this->name = $name;
+        $this->phone = $phone;
+        $this->address = $address;
+    }
+
+    function setName($new_name)
+    {
+        $this->name = $new_description;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setPhone($new_phone)
+    {
+        $this->phone = $new_phone;
+    }
+
+    function getPhone()
+    {
+        return $this->phone;
+    }
+
+    function setAddress($new_address)
+    {
+        $this->address = $new_address;
+    }
+
+    function getAddress()
+    {
+        return $this->address;
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_contacts']
+    }
+
+    static function deleteAll()
+    {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
+    function save()
+    {
+        array_push($_SESSION['list_of_contacts'], $this);
+    }
+}
+?>
