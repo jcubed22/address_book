fb1300f2ff57d84001081e3df1b0a02a83bee126
Jordan Johansen <jordanjohan@gmail.com>
2015-08-07 10:42:29 -0700

    Create contact added page with appropriate routes in app.php.

--- app/app.php
+++ app/app.php
@@ -9,12 +9,22 @@
     }
 
     $app = new Silex\Application();
+    $app['debug'] = true;
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
     ));
 
     $app->get('/', function() use ($app) {
 
         return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
+
+    });
+
+    $app->post('/contact', function() use ($app) {
+
+        $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+        $contact->save();
+        return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
 
     });
 
